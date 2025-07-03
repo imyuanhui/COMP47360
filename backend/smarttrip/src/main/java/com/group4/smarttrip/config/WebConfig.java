491f46582e8ae5b2e7fd259576abca58ce558101
Shelly892 <shuangning.wei@ucdconnect.ie>
2025-07-03 16:48:15 +0100

    fix: localhost config update

--- backend/smarttrip/src/main/java/com/group4/smarttrip/config/WebConfig.java
+++ backend/smarttrip/src/main/java/com/group4/smarttrip/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // allow all endpoints
-                .allowedOrigins("http://137.43.49.24")
+                // .allowedOrigins("http://137.43.49.24")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
