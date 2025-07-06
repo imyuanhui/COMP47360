944ccefc839779be8bb5cfe7ba6edf697a413965
Shelly892 <shuangning.wei@ucdconnect.ie>
2025-07-06 11:06:31 +0100

    chore:update port configs

--- backend/smarttrip/src/main/java/com/group4/smarttrip/config/WebConfig.java
+++ backend/smarttrip/src/main/java/com/group4/smarttrip/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // allow all endpoints
                 // .allowedOrigins("http://137.43.49.24")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","http://137.43.49.24")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
