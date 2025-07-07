1cee5b14cb0dfdf3e9d3f6920e0a814734ee207d
Shelly892 <shuangning.wei@ucdconnect.ie>
2025-07-07 19:28:01 +0100

    config: add ci/cd deployment and hide api key

--- backend/smarttrip/src/main/java/com/group4/smarttrip/config/WebConfig.java
+++ backend/smarttrip/src/main/java/com/group4/smarttrip/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // allow all endpoints
-                .allowedOrigins("http://localhost:5173","https://smarttrip.duckdns.org")
+                .allowedOrigins("https://smarttrip.duckdns.org")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
