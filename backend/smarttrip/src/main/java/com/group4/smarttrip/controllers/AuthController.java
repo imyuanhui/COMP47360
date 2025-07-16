8fd8382deb6efc1d8bcd216a8c451deb338818ad
Shelly892 <shuangning.wei@ucdconnect.ie>
2025-07-16 19:44:56 +0100

    feat: add OAuth2Redirect page and update google login

--- backend/smarttrip/src/main/java/com/group4/smarttrip/controllers/AuthController.java
+++ backend/smarttrip/src/main/java/com/group4/smarttrip/controllers/AuthController.java
@@ -8,6 +8,7 @@ import com.group4.smarttrip.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
