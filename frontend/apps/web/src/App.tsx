dc6da9abc7c258b0006a0aa94a2f366cbec165b5
Shelly892 <shuangning.wei@ucdconnect.ie>
2025-07-18 12:43:29 +0100

    fix: fix oauth file import

--- frontend/apps/web/src/App.tsx
+++ frontend/apps/web/src/App.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast'; 
-// import OAuth2Redirect from './pages/OAuth2Redirect';
 
 // Page Imports
 import LandingPage from './pages/Landingpage';
@@ -11,7 +10,7 @@ import ExplorePlaces from './pages/ExplorePlaces';
 import MyItinerary from './pages/MyItinerary';
 import SavedPlaces from './pages/SavedPlaces';
 import Preferences from './pages/Preferences';
-import OAuth2Redirect from 'pages/Oauth';
+import OAuth2Redirect from './pages/Oauth';
 
 export default function App() {
   return (
@@ -32,12 +31,10 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/myitinerary/:tripId" element={<MyItinerary />} />
         <Route path="/explore/:tripId" element={<ExplorePlaces />} />
-
         <Route path="/itinerary" element={<MyItinerary />} />
         <Route path="/saved/:tripId" element={<SavedPlaces />} />
         <Route path="/oauth-success" element={<OAuth2Redirect />} />
         <Route path="/preferences" element={<Preferences />} />
-        {/* <Route path="/oauth2/redirect" element={<OAuth2Redirect />} /> */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
